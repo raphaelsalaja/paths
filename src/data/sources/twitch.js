80647aac491d77c63ef316464b4ad6ce7b4b8d05
raf-underscore <salajaraphael@gmail.com>
2022-07-08 11:55:17 +0100

    feat: twitch added

---
+++ src/data/sources/twitch.js
@@ -0,0 +1,72 @@
+export const twitch = {
+	title: 'Twitch',
+	icon: 'https://static.twitchcdn.net/assets/favicon-32-e29e246c157142c94346.png',
+	sections: [
+		{
+			name: 'Clip / Video Controls',
+			shortcuts: [
+				{
+					description: 'Pauses/plays a clip or live broadcast.',
+					keys: ['Space'],
+				},
+				{
+					description: 'Pauses/plays a clip or live broadcast.',
+					keys: ['F'],
+				},
+				{
+					description: 'Pauses/plays a clip or live broadcast.',
+					keys: ['Left Arrow'],
+				},
+				{
+					description: 'Pauses/plays a clip or live broadcast.',
+					keys: ['Right Arrow'],
+				},
+				{
+					description: 'Pauses/plays a clip or live broadcast.',
+					keys: ['Alt', 'k'],
+				},
+				{
+					description: 'Pauses/plays a clip or live broadcast.',
+					keys: ['M'],
+				},
+				{
+					description: 'Pauses/plays a clip or live broadcast.',
+					keys: ['Space'],
+				},
+			],
+		},
+		{
+			name: 'Live Broadcast ',
+			shortcuts: [
+				{
+					description: 'Pauses/plays a clip or live broadcast.',
+					keys: ['Space'],
+				},
+				{
+					description: 'Pauses/plays a clip or live broadcast.',
+					keys: ['F'],
+				},
+				{
+					description: 'Pauses/plays a clip or live broadcast.',
+					keys: ['Left Arrow'],
+				},
+				{
+					description: 'Pauses/plays a clip or live broadcast.',
+					keys: ['Right Arrow'],
+				},
+				{
+					description: 'Pauses/plays a clip or live broadcast.',
+					keys: ['Alt', 'k'],
+				},
+				{
+					description: 'Pauses/plays a clip or live broadcast.',
+					keys: ['M'],
+				},
+				{
+					description: 'Pauses/plays a clip or live broadcast.',
+					keys: ['Space'],
+				},
+			],
+		},
+	],
+}
