b3d99a64304d1eb1599f5dd8b7165c835f5b83d8
raf-underscore <salajaraphael@gmail.com>
2022-07-08 19:39:26 +0100

    fix: blurry text
    Closes: #1

--- src/background.js
+++ src/background.js
@@ -18,12 +18,11 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	switch (message.request) {
 		case 'get-shortcuts':
 			sendResponse({shortcuts: sources})
-			return true
 			break
 		case 'get-fact':
-			let fact = dailyFact[Math.floor(Math.random() * dailyFact.length)]
+			console.log(dailyFact)
+			let fact = dailyFact[Math.floor(Math.random() * dailyFact.length())]
 			sendResponse({fact: fact})
-			return true
 			break
 	}
 })
@@ -40,7 +39,9 @@ function setFact() {
 		})
 		.then(() => {
 			chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-				chrome.tabs.sendMessage(tabs[0].id, {quote: dailyFact}, function (response) {})
+				chrome.tabs.sendMessage(tabs[0].id, {quote: dailyFact}, function (response) {
+					console.log(dailyFact)
+				})
 			})
 		})
 }
